a84e21cd0ade238b406caf15cad6010b23bd559b
yirong.zd <yirong.zd@alibaba-inc.com>
2015-06-30 11:29:38 +0800

    whyme init

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+var fs = require("fs"),
+    path = process.cwd();
+
+fs.readdir(path, function(err, files){
+    if(err){
+        return console.log(err);
+    }
+    for(var i = 0; i < files.length; i += 1){
+        console.log(files[i]);
+    }
+});
